c67383ade74c7602e65f0273bfa0dee2783dae00
Ismael Olea <ismael@olea.org>
2014-10-09 10:23:02 +0200

    borrando la versión previa que mal improvisé

--- evento/20141024/index_files/funciones.js
+++
@@ -1,6 +0,0 @@
-//funcion contra el spam
-function antispam(cuenta,clase)
-{
-	var dominio = "hackal.es";
-	document.write("<a class='"+ clase +"' href=\"mailto:" + cuenta + "@" + dominio + "\">" + cuenta + "@" + dominio + "</a>");
-}
